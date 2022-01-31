dba27091fcccf5c03696df2e4237ee130306cc5b
LexinQiu <97572814+LexinQiu@users.noreply.github.com>
2022-01-31 17:40:31 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,11 @@
+
+u = input("Please choose one of : rock, paper, scissors: ")
+print("User Chose:", u)
+
+
+
+import random
+options = ["rock", "paper", "scissors"]
+
+computer_choice = random.choice(options)
+print("COMPUTER CHOSE:", computer_choice)
